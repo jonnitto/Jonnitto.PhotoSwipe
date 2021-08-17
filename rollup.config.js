e2577a8cb9650dc3e955093da275137c9fc76645
Jon Uhlmann <me@jonnitto.ch>
2021-08-17 23:52:21 +0200

    Update: Remove banner

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,6 @@ import { terser } from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import license from 'rollup-plugin-license';
-import composer from './composer.json';
-
-const AUTHOR = composer.authors[0].name;
-const BANNER_CONTENT = `${composer.extra.neos['package-key']} - created by ${AUTHOR}
-@link ${composer.homepage}
-Copyright 2016-${parseInt(new Date().getFullYear(), 10)} ${AUTHOR}
-Licensed under ${composer.license}`;
 
 module.exports = {
     input: 'Resources/Private/Assets/Main.js',
@@ -27,10 +20,6 @@ module.exports = {
             },
         }),
         license({
-            banner: {
-                content: BANNER_CONTENT,
-                commentStyle: 'ignored',
-            },
             thirdParty: {
                 includePrivate: true,
                 output: 'Resources/Public/Main.js.LICENSE',
