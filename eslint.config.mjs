91a356efecfe0c1f954f02cb720fa65cc6cfaa34
Jon Uhlmann <account@uhlmann.pro>
2026-03-11 10:44:22 +0100

    Upgrade: Packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,18 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
     pluginJs.configs.recommended,
     prettierRecommended,
     {
-        ignores: ['Resources/Public/', '*.noLinter.*'],
+        ignores: ["Resources/Public/", "*.noLinter.*"],
         languageOptions: {
             globals: {
                 ...globals.browser,
                 ...globals.node,
-                FLOW: 'readonly',
+                FLOW: "readonly",
             },
         },
-    }
+    },
 ];
